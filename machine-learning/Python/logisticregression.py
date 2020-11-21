ffaf671d7c95e1ec71dce6632a08262ba2fbdb31
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-11-22 00:10:40 +0100

    Update logisticregression.py

--- machine-learning/Python/logisticregression.py
+++ machine-learning/Python/logisticregression.py
@@ -1,14 +1,13 @@
-from __future__ import annotations
 """
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 LOGISTIC REGRESSION
 This is an implementation of the logistic regression.
 It is written in pure Python 3 to be maintainable, portable, and easy to provide to third parties.
 For errors and gradient descent an easy way of implementation has been provided, however, they
 might be overwritten implementing new algorithms and procedures.
+
 Written by Mattia Neroni in July 2020.
 Author's contact : mattianeroni@yahoo.it
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 """
 
 
@@ -16,12 +15,14 @@ Author's contact : mattianeroni@yahoo.it
 """
 Imports from Python 3 standard libraries.
 """
-from typing import List, Tuple, cast
+#from typing import List, Tuple, cast
 
 import numpy # type: ignore
 import math
 import statistics
 import random
+import array
+import matplotlib.pyplot as plt
 
 
 
@@ -31,15 +32,15 @@ import random
 
 
 
-def prepare_test (X : List[List[float]], y : List[int], test_size : float = 0.3) -> Tuple[List[List[float]], List[List[float]], List[int], List[int]]:
+def prepare_test (X, y, test_size = 0.3):
     """
     Given a set of input data, and a set of output data, this method split them
     according to the size defined.
     Part of the data is placed in the test dataset, the remaining part in the 
     training dataset.
-    :param X: The matrix of input data
-    :param y: The correct outputs
-    :param test_size: The percentage of the total data, which is kept for the tests
+    :param X: <list<list>>The matrix of input data
+    :param y: <list> The correct outputs
+    :param test_size: <float> The percentage of the total data, which is kept for the tests
     :return: <tuple> Input data to use in the tests, Input data to use in the training,
                      correct outputs for the tests, correct outputs for the training.
     """
@@ -67,20 +68,16 @@ def prepare_test (X : List[List[float]], y : List[int], test_size : float = 0.3)
 
 
 class LogisticRegression:
-
     """
     This is the logistic regression class. 
     Before being used, it must be instantiated.
     """
-
-
-
-    def __init__ (self, learning_rate : float = 0.01, num_iter : int = 30_000, verbose : bool = False) -> None:
+    def __init__ (self, learning_rate = 0.01, num_iter = 30_000, verbose = False):
         """
         Initialize.
-        :param learning_rate: The learning rate of the regression
-        :param num_iter: The number of iterations used in the training
-        :param verbose: If True, during the training, the regression prints all 
+        :param learning_rate: <float> The learning rate of the regression
+        :param num_iter: <int> The number of iterations used in the training
+        :param verbose: <bool> If True, during the training, the regression prints all 
                         the results to show the improvement.
         :attr weights: The weights (i.e. thetas) on the entering edges.
         """
@@ -88,23 +85,23 @@ class LogisticRegression:
         self.num_iter = num_iter
         self.verbose = verbose
 
-        self.weights : List[float]
+        self.weights = None
 
 
 
 
 
     @staticmethod
-    def _sigmoid (x : List[float], weights : List[float]) -> float:
+    def _sigmoid (x, weights):
         """
         This method, given an array of inputs and the array of the weights, 
         returns the response of the regression by adopting a sigmoid
         function.
-        :param x: Array of inputs
-        :param weights: Array of weights in the respective entering edges.
-        :return: The prediction of the regression.
+        :param x: <array> Array of inputs
+        :param weights: <array> Array of weights in the respective entering edges.
+        :return: <float> The prediction of the regression.
         """
-        z : float = numpy.dot(numpy.transpose(x), weights)
+        z = numpy.matmul(numpy.transpose(x), weights)
         return 1 / (1 + numpy.exp(-z))
 
 
@@ -113,7 +110,7 @@ class LogisticRegression:
     
     
 
-    def prediction (self, x : List[float]) -> int:
+    def prediction (self, x):
         """
         This method is used for the prevision of a single element by using the current weights.
         :param x: <list> The input data necessary for the prevision.
@@ -127,18 +124,18 @@ class LogisticRegression:
     
     
     @staticmethod
-    def _fuzzy_prediction (X : List[List[float]], weights : List[float]) -> List[float]:
+    def _fuzzy_prediction (X, weights):
         """
         This method makes the predictions used for training. It takes a long
         set of inputs, and, by using the sigmoid function, it returns the 
         probabilities associated to each output.
         
-        :param X: The matrix of inputs
-        :param weights: The weights on the edges
-        :return: The array of the predictions
+        :param X: <list<list>> The matrix of inputs
+        :param weights: <list> The weights on the edges
+        :return: <list> The array of the predictions
         
         """
-        return 1 / (1 + numpy.exp(- numpy.dot (X, weights)))
+        return 1 / (1 + numpy.exp(- numpy.matmul (weights, X)))
     
     
     
@@ -148,7 +145,7 @@ class LogisticRegression:
 
 
     @staticmethod
-    def _get_loss (predictions : List[float], results : List[int]) -> float:
+    def _get_loss (predictions, results):
         """
         Once a prediction has been made, this method evaluates its quality.
         Several different indexes might be used to evaluate the prediction, but in this 
@@ -173,7 +170,7 @@ class LogisticRegression:
 
 
     @staticmethod
-    def _get_gradient (X : List[List[float]], predictions : List[float], results : List[int]) -> List[float]:
+    def _get_gradient (X, predictions, results):
         """
         This method calculates the gradient, which is the partial derivative of the 
         of the total error made by the prediction.
@@ -182,8 +179,8 @@ class LogisticRegression:
         :param results: The array of the results the prediction was supposed to provide.
         :result: The intensity and the directions according to which each weight is going to be changed.
         """
-        errors_with_sign : List[float] = numpy.asarray(predictions) - numpy.asarray(results)
-        return numpy.dot(numpy.transpose(X), errors_with_sign) / len(results)
+        errors_with_sign = numpy.asarray(predictions) - numpy.asarray(results)
+        return numpy.matmul(numpy.transpose(X), errors_with_sign) / len(results)
 
 
 
@@ -191,7 +188,7 @@ class LogisticRegression:
     
     
     @staticmethod
-    def _add_intercept (X : List[List[float]]) -> List[List[float]]:
+    def _add_intercept (X):
         """
         This method adds an array of ones in front of each array of inputs
         and, in this way, it allows the use of the intercept, which makes the
@@ -209,15 +206,15 @@ class LogisticRegression:
     
     
 
-    def fit (self, X : List[List[float]], y : List[int]) -> None:
+    def fit (self, X, y):
         """
         In this method is written the training procedure based on the Gradient Descendent 
         algorithm.
         :param X: The matrix of the input data.
         :param y: The expected predictions.
         """
-        
-        self.weights = [random.random() for i in range(len(X[0]) + 1)]
+        history = array.array('d',[0.0 for _ in range(self.num_iter // 1000)])
+        self.weights = np.random.rand(len(X[0]) + 1)
         X = self._add_intercept (X)
 
         for i in range(self.num_iter):
@@ -225,8 +222,12 @@ class LogisticRegression:
             gradient = self._get_gradient (X, h, y)
             self.weights = numpy.asarray(self.weights) - self.lr * gradient
 
-            if self.verbose is True and i % 1000 == 0:
-                print("-> Iteration : ", i, ", Loss : ", self._get_loss(h, y))
+            if i % 1000 == 0:
+                history[i // 1000] = self._get_loss(h, y))
+                
+        if self.verbose is True:
+            plt.plot(history)
+            plt.show()
 
 
 
@@ -234,7 +235,7 @@ class LogisticRegression:
 
 
 
-    def test (self, X : List[List[float]], y : List[int]) -> Tuple[float, List[int]]:
+    def test (self, X, y):
         """
         This method test the regression on a dataset with the current weights and 
         finally returns the percentage error and the answers provided by the 
@@ -243,14 +244,14 @@ class LogisticRegression:
         :param y: The correct answers
         :return: <tuple> The percentage error and the previsions made by the regression.
         """
-        previsions : List[int] = []
+        previsions = array.array('i', [-1 for _ in range(len(y))])
         error_num : int = 0
             
-        for x, answer in zip (X, y):
+        for i, (x, answer) in enumerate(zip (X, y)):
             
             x = numpy.insert (x, 0, 1)
             p = self.prediction (x)
-            previsions.append(p)
+            previsions[i] = p
 
             if p != answer:
                 error_num += 1
