594ae6aad55c69b3414f6eb0d6e3470966aa8238
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-11-21 19:15:07 +0100

    Update sorting.py

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,28 +1,25 @@
-from __future__ import annotations
-
-from typing import List
+import itertools
 
 
-
-def quicksort (array : List[int]) -> List[int]:
+def quicksort (array):
+	"""
+	Quicksort algorithm.
+	
+	"""
 	if len (array) <= 1:
 		return array
 	p, i, j = 0, 0, len(array)-1
 	while i < j:
 		if i == p:
 			if array[p] > array[j]:
-				array[j] += array[p]
-				array[p] = array[j] - array[p]
-				array[j] -= array[p]
+				array[j], array[p] = array[p], array[j]
 				p = j
 			else:
 				j -= 1
 		if j == p:
 			if array[p] < array[i]:
-				array[i] += array[p]
-				array[p] = array[i] - array[p]
-				array[i] -= array[p]
+				array[i], array[p] = array[p], array[i]
 				p = i
 			else:
 				i += 1
-	return quicksort(array[:p]) + [array[p]] + quicksort(array[p+1:])
+	return itertools.chain (quicksort(array[:p]), [array[p]], quicksort(array[p+1:]))
