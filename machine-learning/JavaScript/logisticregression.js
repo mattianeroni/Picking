2c3a4464160284b5ab07034f27d20f68b63d422b
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-11-30 23:52:48 +0100

    Update logisticregression.js

--- machine-learning/JavaScript/logisticregression.js
+++ machine-learning/JavaScript/logisticregression.js
@@ -1,4 +1,11 @@
 class LogisticRegression {
-
+  constructor (inputs) {
+      this.inputs = inputs;
+      var weights = [];
+      for (var i = 0; i < inputs; i++){
+         weights.push (Math.random());
+      }
+      this.weights = weights;
+  }
 
 }
